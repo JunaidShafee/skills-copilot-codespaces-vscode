1978f14e5c7b272307dd8d8064e15bc96b57254d
JunaidShafee <144826049+JunaidShafee@users.noreply.github.com>
2023-09-12 16:21:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember(){
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html'
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        binToController: true,
+        scope: {
+            member: '='
+        }
+    };
+}
